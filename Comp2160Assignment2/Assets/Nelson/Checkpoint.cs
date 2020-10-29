16030d76771b79a6a0f366c0b2bb029d2ddf4346
mqnelsont <54386742+mqnelsont@users.noreply.github.com>
2020-10-29 20:07:13 +1100

    Checkpoint can turn on and off. Checkpoint Manager handles the checkpoints.

---
+++ Comp2160Assignment2/Assets/Nelson/Checkpoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    Light light;
+    public float lightIntensity = 10f;
+
+    void Start()
+    {
+        light = GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeactivateLight()
+    {
+        light.intensity = 0;
+
+    }
+
+    public void ActivateLight()
+    {
+        light.intensity = lightIntensity;
+    }
+}
---
+++ Comp2160Assignment2/Assets/Nelson/CheckpointManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+
+    public List<Checkpoint> checkpoints = new List<Checkpoint>();
+
+    int checkpointCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (checkpointCount >= checkpoints.Count)
+            {
+                Debug.Log("completed");
+            }
+            else
+            {
+                if (checkpointCount == 0)
+                {
+                    checkpoints[checkpointCount].ActivateLight();
+                    checkpointCount++;
+                }
+                else
+                {
+                    checkpoints[checkpointCount].ActivateLight();
+                    checkpoints[checkpointCount - 1].DeactivateLight();
+                    checkpointCount++;
+                }
+            }
+        }
+    }
+}
